8e8907b58cb70a633ac24657377a435a7fdc502d
Princenick-BL <princenickballo@gmail.com>
2022-08-06 21:58:49 +0200

    updates

--- components/StoriesWidget/index.js
+++ components/StoriesWidget/index.js
@@ -16,72 +16,75 @@ export default function StoriesWidget() {
 
     return (
       <div className={styles.container}>
-        <div className={styles.list}>
-            {stories ? stories?.map((story,index)=>{
-              return(
-                  <a key={index} href={`/web-story/${story?._id}/${story?.slug}`} target={"_blank"} rel="noreferrer">
-                    <div  className={styles.outerCorner}>
-                        <div 
-                            className={styles.innerConner}
-                            style={{backgroundImage:`url(${story?.poster})`}}
-                        ></div>
-                    </div>
+        <div className={styles.box}>
+          <h1>Stories</h1>
+          <div className={styles.list}>
+              {stories ? stories?.map((story,index)=>{
+                return(
+                    <a key={index} href={`/web-story/${story?._id}/${story?.slug}`} target={"_blank"} rel="noreferrer">
+                      <div  className={styles.outerCorner}>
+                          <div 
+                              className={styles.innerConner}
+                              style={{backgroundImage:`url(${story?.poster})`}}
+                          ></div>
+                      </div>
 
-                  </a>
-                
-              )
-            }):(
-              <Loading/>
-            )}
-            {stories ? stories?.map((story,index)=>{
-              return(
-                  <a key={index} href={`/web-story/${story?._id}/${story?.slug}`} target={"_blank"} rel="noreferrer">
-                    <div  className={styles.outerCorner}>
-                        <div 
-                            className={styles.innerConner}
-                            style={{backgroundImage:`url(${story?.poster})`}}
-                        ></div>
-                    </div>
+                    </a>
+                  
+                )
+              }):(
+                <Loading/>
+              )}
+              {stories ? stories?.map((story,index)=>{
+                return(
+                    <a key={index} href={`/web-story/${story?._id}/${story?.slug}`} target={"_blank"} rel="noreferrer">
+                      <div  className={styles.outerCorner}>
+                          <div 
+                              className={styles.innerConner}
+                              style={{backgroundImage:`url(${story?.poster})`}}
+                          ></div>
+                      </div>
 
-                  </a>
-                
-              )
-            }):(
-              <Loading/>
-            )}
-            {stories ? stories?.map((story,index)=>{
-              return(
-                  <a key={index} href={`/web-story/${story?._id}/${story?.slug}`} target={"_blank"} rel="noreferrer">
-                    <div  className={styles.outerCorner}>
-                        <div 
-                            className={styles.innerConner}
-                            style={{backgroundImage:`url(${story?.poster})`}}
-                        ></div>
-                    </div>
+                    </a>
+                  
+                )
+              }):(
+                <Loading/>
+              )}
+              {stories ? stories?.map((story,index)=>{
+                return(
+                    <a key={index} href={`/web-story/${story?._id}/${story?.slug}`} target={"_blank"} rel="noreferrer">
+                      <div  className={styles.outerCorner}>
+                          <div 
+                              className={styles.innerConner}
+                              style={{backgroundImage:`url(${story?.poster})`}}
+                          ></div>
+                      </div>
 
-                  </a>
-                
-              )
-            }):(
-              <Loading/>
-            )}
-            {stories ? stories?.map((story,index)=>{
-              return(
-                  <a key={index} href={`/web-story/${story?._id}/${story?.slug}`} target={"_blank"} rel="noreferrer">
-                    <div  className={styles.outerCorner}>
-                        <div 
-                            className={styles.innerConner}
-                            style={{backgroundImage:`url(${story?.poster})`}}
-                        ></div>
-                    </div>
+                    </a>
+                  
+                )
+              }):(
+                <Loading/>
+              )}
+              {stories ? stories?.map((story,index)=>{
+                return(
+                    <a key={index} href={`/web-story/${story?._id}/${story?.slug}`} target={"_blank"} rel="noreferrer">
+                      <div  className={styles.outerCorner}>
+                          <div 
+                              className={styles.innerConner}
+                              style={{backgroundImage:`url(${story?.poster})`}}
+                          ></div>
+                      </div>
 
-                  </a>
-                
-              )
-            }):(
-              <Loading/>
-            )}
-            
+                    </a>
+                  
+                )
+              }):(
+                <Loading/>
+              )}
+              
+          </div>
         </div>
       </div>
     )
